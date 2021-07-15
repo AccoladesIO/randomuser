31b20ba1a3ec0845e9082b81d0f7324327321581
Karrel E. Catid <karrelecatid@su.edu.ph>
2021-07-15 10:39:02 +0800

    Update: added plugins for primevue

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,9 @@ import { createApp } from 'vue';
 import App from './app.vue';
 import router from './router';
 import PrimeVue from 'primevue/config';
-import 'primevue/resources/themes/bootstrap4-light-blue/theme.css';
-import 'primevue/resources/primevue.min.css';
-import 'primeicons/primeicons.css';
+import '@/plugins/primevue';
 
 const app = createApp(App);
 app.use(PrimeVue);
-
-createApp(App).use(router).mount('#app');
+app.use(router);
+app.mount('#app');
